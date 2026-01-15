46edcfc2888af020334146ca970ecb4064a0adfe
ericeric <carescor998@gmail.com>
2026-01-15 00:58:55 -0300

    Add logs routes

---
+++ log/logs.routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('./logs.controller');
+const db = require('../dbUsers')
+
+router.get('/', controller.obternerLogs);
+
+router.get('/ultimos', controller.ultimosLogs);
+
+router.post('/', controller.hacerLog);
+
+module.exports = router;

37c710e908e2d8bde6f7c88814187d8b40f825dc
ericeric <carescor998@gmail.com>
2026-01-18 18:27:31 -0300

    Remove connection logging and test queries from database pool configurations for alumnos and usuarios

--- dbDatos.js
+++ dbDatos.js
@@ -9,23 +9,5 @@ const poolDatos = new Pool({
   port: process.env.DB_DATOS_PORT || 5432,
 });
 
-// Loguear cuando el pool establece una conexión
-poolDatos.on('connect', () => {
-  console.log('DB Alumonos: conexión establecida');
-});
-
-// Loguear errores del pool
-poolDatos.on('error', (err) => {
-  console.error('DB alumnos: error en el pool', err);
-});
-
-// Prueba rápida de conexión al cargar el módulo
-poolDatos.query('SELECT 1')
-  .then(() => {
-    console.log('DB alumnos: prueba de conexión exitosa');
-  })
-  .catch((err) => {
-    console.error('DB alumnos: prueba de conexión fallida', err);
-  });
 
 module.exports = poolDatos;
--- dbUsers.js
+++ dbUsers.js
@@ -8,23 +8,6 @@ const poolUsuarios = new Pool({
   password: process.env.DB_USUARIOS_PASS,
   port: process.env.DB_USUARIOS_PORT || 5432,
 });
-// Loguear cuando el pool establece una conexión
-poolUsuarios.on('connect', () => {
-  console.log('DB Usuarios: conexión establecida');
-});
 
-// Loguear errores del pool
-poolUsuarios.on('error', (err) => {
-  console.error('DB Usuarios: error en el pool', err);
-});
-
-// Prueba rápida de conexión al cargar el módulo
-poolUsuarios.query('SELECT 1')
-  .then(() => {
-    console.log('DB Usuarios: prueba de conexión exitosa');
-  })
-  .catch((err) => {
-    console.error('DB Usuarios: prueba de conexión fallida', err);
-  });
 
 module.exports = poolUsuarios;

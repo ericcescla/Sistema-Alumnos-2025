829c89139f81a19f7cdd646d2c901fba5cf38808
ericeric <carescor998@gmail.com>
2026-01-28 12:51:12 -0300

    Problem solved in createCourse service

---
+++ cursos/cursos.service.js
@@ -0,0 +1,32 @@
+const repo = require('./cursos.repository');
+
+async function obtenerCursos() {
+    const result = await repo.obternerCursos();
+    return result.rows;
+}
+
+async function crearCurso(anio, division, id_plan, anio_lectivo) {
+    const { rows } = await repo.findCursoExistente(
+        anio_lectivo, anio, division,
+    );
+    console.log(rows);
+
+    if (rows[0].exists) {
+        throw new Error(`El curso ${anio}${division} del año ${anio_lectivo} ya existe`);
+    }
+
+
+    await repo.crearCurso(anio, division, id_plan, anio_lectivo);
+
+    return "Curso insertado correctamente";
+}
+
+
+async function alumnosPorCurso(anioLectivo, anio, division) {
+    const result = await repo.alumnosPorCurso(anioLectivo, anio, division);
+    return result.rows;
+}
+
+module.exports = {
+    obtenerCursos, crearCurso, alumnosPorCurso
+}
